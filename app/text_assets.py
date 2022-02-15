2aef74bbb62a91df6b1f3c0dde3b0d37f3519577
ljdyer <74581995+ljdyer@users.noreply.github.com>
2022-02-15 12:10:37 +0000

    Update text_assets.py

--- app/text_assets.py
+++ app/text_assets.py
@@ -11,5 +11,12 @@ blurb = """
 You be Mark Lamaar. I'm the guest. You say a line from a song, and I'll tell
 you the next line, the song title, and the artist.<br><br>
 
-For example, try: "When I'm not with you I lose my mind, give me a sign"
+I'll be mostly scraping the website
+<a target="_blank" href="https://songsear.ch/">songsear.ch</a>,
+parsing results using the BeautifulSoup library, and carrying out fuzzy
+string matching using the FuzzyWuzzy library in Python. I get it wrong
+sometimes and you can break me if you try, but I'll get better in the future.<br><br>
+
+For best results, write a single, complete line from a well-known song.
+For example, try: "When I'm not with you I lose my mind, give me a sign".
 """
